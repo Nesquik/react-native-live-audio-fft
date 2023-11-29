f36ce56492b6917f3654c175b38cc30a7a021177
Li Zheng <flyskywhy@gmail.com>
2023-11-29 15:21:47 +0800

    Change audioSource of Android from VOICE_RECOGNITION to MIC, because MIC is better for music frequency histogram

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ function NativeRecordReceivePCM(pcmDataBase64) {
 //   sampleRate: 32000,  // default is 44100 but 32000 is adequate for accurate voice recognition
 //   channels: 1,        // 1 or 2, default 1
 //   bitsPerSample: 16,  // 8 or 16, default 16
-//   audioSource: 6,     // android only (see below)
+//   audioSource: 1,     // android only (see below)
 //   bufferSize: 4096    // default is 2048
 // };
 //
